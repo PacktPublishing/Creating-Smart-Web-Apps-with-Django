863e7da99b9a2d2de6129632cd7893284c4b3464
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2018-05-31 00:21:35 +0530

    blog: Add model Blogpost

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blogpost(models.Model):
+    title = models.CharField(max_length=240)
+    author = models.CharField(max_length=240, db_index=True)
+    body = models.TextField()
+
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
