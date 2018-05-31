d8e310bb3ac7b11523f63e9e7bd4c3f40a6e5ec7
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2018-05-31 23:39:00 +0530

    blog: Add view to list posts

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+
+from blog.models import Blogpost
 
 # Create your views here.
+
+class BlogpostView(View):
+    def get(self, request):
+        posts = Blogpost.objects.all()
+
+        response = [
+            # 1: first post by John Doe
+            "{id}: {title} by {author} <br>".format(id=p.id, title=p.title, author=p.author)
+            for p in posts
+        ]
+
+        return HttpResponse(response)
--- website/website/urls.py
+++ website/website/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from blog.views import BlogpostView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', BlogpostView.as_view(), name='posts'),
 ]
