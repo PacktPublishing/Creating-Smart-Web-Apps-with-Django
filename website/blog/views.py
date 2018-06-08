6cab00ec4a78f598b8ed428b5b67c15c05617edf
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2018-06-09 00:17:49 +0530

    blog: Use django templates

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,35 +1,38 @@
 from django.http import HttpResponse, Http404
-from django.views.generic import View
+from django.views.generic import TemplateView
 
 from blog.models import Blogpost
 
 # Create your views here.
 
-class BlogpostView(View):
+class BlogpostView(TemplateView):
+    template_name = 'blog/index.html'
+
     def get(self, request):
         posts = Blogpost.objects.all()
 
-        response = [
-            # 1: first post by John Doe
-            "{id}: {title} by {author} <br>".format(id=p.id, title=p.title, author=p.author)
-            for p in posts
-        ]
+        response = [{
+            'id': p.id,
+            'title': p.title,
+            'author': p.author,
+        } for p in posts]
 
-        return HttpResponse(response)
+        return self.render_to_response({'posts': response})
 
-class BlogpostDetailView(View):
+
+class BlogpostDetailView(TemplateView):
+    template_name = 'blog/detail.html'
+
     def get(self, request, id):
         try:
             p = Blogpost.objects.get(id=id)
         except Blogpost.DoesNotExist:
             raise Http404()
         else:
-            response = """
-            {title} by {author}
-            <br>
-            ---
-            <br><br>
-            {body}
-            """.format(title=p.title, author=p.author, body=p.body)
+            context = {
+                'title': p.title,
+                'author': p.author,
+                'body': p.body,
+            }
 
-            return HttpResponse(response)
+            return self.render_to_response(context)
