8ecb9c15b649432f3371790945fc44bdec70dae5
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2018-08-01 20:45:19 +0530

    blog: Add field user to model Blog
    This is step 1 of a series of steps for a zero downtime data
    migration. Here we leave the default value of the new field as empty
    to avoid a time consuming database transaction in a production
    environment.

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
 
 class Blogpost(models.Model):
     title = models.CharField(max_length=240)
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True, default=None)
+
     author = models.CharField(max_length=240, db_index=True)
     body = models.TextField()
 
