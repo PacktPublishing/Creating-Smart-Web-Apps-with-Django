30d6045ac53f2df50b5a469173b1480d5b7d8176
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2018-08-25 19:27:14 +0530

    blog: Register BlogpostAdmin

--- website/blog/admin.py
+++ website/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from blog.models import Blogpost
+
 # Register your models here.
+
+
+class BlogpostAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Blogpost, BlogpostAdmin)
